38c6965ca9479db7a83f66b13f8d978eab83c8bc
santiago <santtyzelada@gmail.com>
2024-03-30 14:01:26 +0100

    Renderizado por primera vez de componente Pagina Principal

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,26 @@
-import "bootstrap/dist/css/bootstrap.min.css";
-import "bootstrap-icons/font/bootstrap-icons.min.css";
+// import "bootstrap/dist/css/bootstrap.min.css";
+// import "bootstrap-icons/font/bootstrap-icons.min.css";
 import "./App.css";
-import Footer from "./components/common/Footer";
-import Menu from "./components/common/Menu";
-import Error404 from "./components/pages/Error404";
-import Administrador from "./components/pages/Administrador";
+// import Footer from "./components/common/Footer";
+// import Menu from "./components/common/Menu";
+// import Error404 from "./components/pages/Error404";
+// import Administrador from "./components/pages/Administrador";
+// import FormularioProducto from "./components/pages/producto/FormularioProducto";
+// import { BrowserRouter, Routes, Route } from "react-router-dom";
+// import Registro from "./components/pages/Registro";
+// import Login from "./components/pages/Login";
+// import RutasProtegidas from "./components/routes/RutasProtegidas";
+// import RutasAdmin from "./components/routes/RutasAdmin";
+// import { useState } from "react";
+
 import Inicio from "./components/pages/Inicio";
-import FormularioProducto from "./components/pages/producto/FormularioProducto";
-import DetalleProducto from "./components/pages/DetalleProducto";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Registro from "./components/pages/Registro";
-import Login from "./components/pages/Login";
-import RutasProtegidas from "./components/routes/RutasProtegidas";
-import RutasAdmin from "./components/routes/RutasAdmin";
-import { useState } from "react";
+
 
 function App() {
   
 
   return (
-    <BrowserRouter>
-      
-    </BrowserRouter>
+    <Inicio></Inicio>
   );
 }
 
---
+++ src/components/pages/Inicio.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import './styles/Inicio.css'
+
+const Inicio = () => {
+    return <>
+        <section className="backgroundInicio">
+            <section className="Entradas"></section>
+            <section className="PlatillosPrincipales"></section>
+            <section className="Bebidas"></section>
+            <section className="Postres"></section>
+        </section>
+    </>
+}
+
+export default Inicio;
