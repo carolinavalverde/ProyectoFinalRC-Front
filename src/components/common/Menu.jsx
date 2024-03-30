213eb852f36035a3617377a3f89ba7c2c9ef330a
Caro Valverde <carolina.valverde255@gmail.com>
2024-03-30 19:12:31 -0300

    agrego componente navbar

---
+++ src/components/common/Menu.jsx
@@ -0,0 +1,9 @@
+import { Navbar, Nav, Container, Button } from "react-bootstrap";
+import logo from "../../assets/coffee_Logo.png";
+import { NavLink, Link, useNavigate } from "react-router-dom";
+
+const Menu = () => {};
+
+return <Navbar expand="lg" className="bg-body-tertiary"></Navbar>;
+
+export default Menu;
