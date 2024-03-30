3da381cdefcd2c66545c165536fc7242e49446dc
Caro Valverde <carolina.valverde255@gmail.com>
2024-03-30 15:43:35 -0300

    agrego rutas administrador

---
+++ src/components/routes/RutasAdmin.jsx
@@ -0,0 +1,24 @@
+import { Routes, Route } from "react-router-dom";
+import Administrador from "../pages/Administrador";
+import FormularioProducto from "../pages/producto/FormularioProducto";
+
+const RutasAdmin = () => {
+  return (
+    <Routes>
+      <Route exact path="/" element={<Administrador></Administrador>}></Route>
+    
+      <Route
+        exact
+        path="/editar/:id"
+        element={
+          <FormularioProducto
+            editando={true}
+            titulo="Editar producto"
+          ></FormularioProducto>
+        }
+      ></Route>
+    </Routes>
+  );
+};
+
+export default RutasAdmin;
