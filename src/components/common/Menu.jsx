4d7467f631ac381e7f150eaecfca7c96f5ece1c5
Caro Valverde <carolina.valverde255@gmail.com>
2024-03-31 16:06:12 -0300

    agrego css

--- src/components/common/Menu.jsx
+++ src/components/common/Menu.jsx
@@ -1,9 +1,8 @@
 import { Navbar, Nav, Container, Button } from "react-bootstrap";
-import logo from "../../assets/coffee_Logo.png";
 import { NavLink, Link, useNavigate } from "react-router-dom";
 
 const Menu = () => {};
 
-return <Navbar expand="lg" className="bg-body-tertiary"></Navbar>;
+return <Navbar></Navbar>;
 
 export default Menu;
--- src/helpers/queries.js
+++ src/helpers/queries.js
@@ -26,3 +26,70 @@ export const crearProducto = async (productoNuevo) => {
     console.log(error);
   }
 };
+
+
+//
+
+//DELETE
+export const borrarProducto = async (id) => {
+  try {
+    const respuesta = await fetch(APIProductos + "/" + id, {
+      method: "DELETE",
+    });
+    console.log(respuesta);
+    return respuesta;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+//GET de un unico producto
+export const obtenerProducto = async (id) => {
+  try {
+    const respuesta = await fetch(APIProductos + "/"+ id);
+    console.log(respuesta);
+    return respuesta;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+// queries.js
+
+export const editarProducto = async (id, datos) => {
+  // Código para editar el producto con el id dado usando los datos proporcionados
+};
+
+
+//cuando tengamos el backend con un login enviar solicitud POST
+const userAdmin = {
+  email: "admin@rollingcoffee.com",
+  password: "123Aa$123",
+};
+
+export const login = (usuario) => {
+  if (
+    usuario.email === userAdmin.email &&
+    usuario.password === userAdmin.password
+  ) {
+    sessionStorage.setItem("loginRollingCoffee", JSON.stringify(usuario.email));
+    return true;
+  } else {
+    return false;
+  }
+};
+
+export const registrarUsuario = async (usuario) => {
+  try {
+    const respuesta = await fetch(APIRegistro, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(usuario),
+    });
+    return respuesta;
+  } catch (error) {
+    console.log(error);
+  }
+};
