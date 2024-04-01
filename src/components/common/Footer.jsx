32a74b33b6eb3f5ebd9934ad95144f33794b030d
Caro Valverde <carolina.valverde255@gmail.com>
2024-04-01 16:52:51 -0300

    agrego componente footer

---
+++ src/components/common/Footer.jsx
@@ -0,0 +1,14 @@
+import { Link } from "react-router-dom";
+import "../../style/Footer.css";
+
+const Footer = () => {
+  return (
+    <footer className="py-3 py-4 BgFooter">
+      <div className="container text-center text-md-start">
+       
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
