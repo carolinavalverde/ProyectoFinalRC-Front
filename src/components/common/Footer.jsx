63d7c02ad8e23178a311d561183f0e36fced1ef5
Caro Valverde <carolina.valverde255@gmail.com>
2024-04-01 16:57:18 -0300

    agrego css y corrijo carpeta de css con links en  footer, nav y admin

--- src/components/common/Footer.jsx
+++ src/components/common/Footer.jsx
@@ -1,11 +1,21 @@
 import { Link } from "react-router-dom";
-import "../../style/Footer.css";
+import "../../styles/Footer.css";
 
 const Footer = () => {
   return (
     <footer className="py-3 py-4 BgFooter">
       <div className="container text-center text-md-start">
-       
+        <div className="row">
+          <section className="col-md-5 col-lg-4 text-center text-lg-start">
+          
+          </section>
+          <section className="col-md-4 col-lg-4 TextoFooter text-center">
+           
+          </section>
+          <section className="col-md-3 col-lg-4 TextoFooter">
+           
+          </section>
+        </div>
       </div>
     </footer>
   );
