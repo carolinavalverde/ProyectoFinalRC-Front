bec9744a71c8c68ad839feafc4ff25d29d911668
Caro Valverde <carolina.valverde255@gmail.com>
2024-04-01 16:59:35 -0300

    agrego section de horarios y contacto

--- src/components/common/Footer.jsx
+++ src/components/common/Footer.jsx
@@ -13,7 +13,26 @@ const Footer = () => {
               </div>
             </Link>
           </section>
-          <section className="col-md-4 col-lg-4 TextoFooter text-center"></section>
+          <section className="col-md-4 col-lg-4 TextoFooter text-center">
+            {" "}
+            <div>
+              <h6>CONTACTANOS</h6>
+              <p>Jueves a Domingos de 20pm a 02am</p>
+              <ul className="list-group-item">
+                <li className="list-group-item mb-2">
+                  <i className="bi bi-telephone-fill"></i> 381-21764295
+                </li>
+                <li className="list-group-item mb-2">
+                  <i className="bi bi-envelope-fill"></i>{" "}
+                  vermont.restaurant@gmail.com
+                </li>
+                <li className="list-group-item mb-2">
+                  <i className="bi bi-geo-alt-fill"></i> Gral. Paz 576, San
+                  Miguel de Tucumán
+                </li>
+              </ul>
+            </div>
+          </section>
           <section className="col-md-3 col-lg-4 TextoFooter"></section>
         </div>
       </div>
