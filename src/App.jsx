35381168f91375e045fc7ce6a6ceb42cf1f145cc
santiago <santtyzelada@gmail.com>
2024-04-03 11:44:34 +0200

    Movi los componentes a la carpeta Menu para que todo quede más organizado y arregle las rutas de cg2e

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,15 @@
-// import "bootstrap/dist/css/bootstrap.min.css";
-// import "bootstrap-icons/font/bootstrap-icons.min.css";
+import React from "react";
 import "./App.css";
-import MenuCategorias from "./components/pages/MenuCategorias";
-import Button from 'react-bootstrap/Button'
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { Carousel } from "bootstrap";
-import CarouselPage from "./components/pages/CarouselPage";
+// import MenuCategorias from "./components/pages/Menu/MenuCategorias";
+// import 'bootstrap/dist/css/bootstrap.min.css';
+// import { Carousel } from "bootstrap";
+// import CarouselPage from "./components/pages/Menu/CarouselPage";
 
+import PaginaMenuCategorias from "./components/pages/Menu/PaginaMenuCategorias";
 function App() {
 
   return (
-    <div className="itemsContainer">
-      <hr/>
-      <CarouselPage></CarouselPage>
-      <hr/>
-
-      <h2 style={{ fontWeight: '100', fontSize: '3rem', textTransform: 'uppercase' }}>Entradas</h2>
-      <MenuCategorias />
-      <hr />
-      <h2 style={{ fontWeight: '100', fontSize: '3rem', textTransform: 'uppercase' }}>Platillos</h2>
-      <MenuCategorias />
-      <hr />
-
-      <h2 style={{ fontWeight: '100', fontSize: '3rem', textTransform: 'uppercase' }}>Bebidas</h2>
-      <MenuCategorias />
-      <hr />
-
-      <h2 style={{ fontWeight: '100', fontSize: '3rem', textTransform: 'uppercase' }}>Postres</h2>
-      <MenuCategorias />
-    </div>
+    <PaginaMenuCategorias></PaginaMenuCategorias>
   )
 
 }
