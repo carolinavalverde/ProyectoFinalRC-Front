2ced2cba93d40ace170bd8a4deebd57be63d02ba
santiago <santtyzelada@gmail.com>
2024-04-03 21:02:08 +0200

    Corregi el archivo Delete.jsx y pase el codigo al archivo queries.js

--- src/helpers/queries.js
+++ src/helpers/queries.js
@@ -28,3 +28,16 @@ export const crearProducto = async (productoNuevo) => {
 
 };
 
+
+export const borrarProducto = async (id) => {
+    try {
+        const respuesta = await fetch(APIProductos + "/" + id, {
+            method: "DELETE",
+        });
+        console.log(respuesta);
+        return respuesta;
+    } catch (error) {
+        console.log(error);
+    }
+};
+
