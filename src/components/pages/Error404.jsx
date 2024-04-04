3786d274e9c60c9010ac28808ec9e07a770d279f
Figueroa Fernando <feroa88@gmail.com>
2024-04-04 09:34:14 -0300

    maquetado de pagina de error

---
+++ src/components/pages/Error404.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Container } from 'react-bootstrap';
+import "./Error404.css";
+
+const Error404 = () => {
+    return (
+        <main className="mainSection main-error">
+      <Container>
+        <div className="text-center my-5">
+          <img
+            src="https://media2.giphy.com/media/UoeaPqYrimha6rdTFV/giphy.gif?cid=ecf05e47ngu625ggear5pm8umrg5i33la76ug6hveupdvjid&ep=v1_gifs_related&rid=giphy.gif&ct=g"
+            alt="imagen de error 404"
+            className="img-fluid"
+          />
+        </div>
+      </Container>
+    </main>
+            
+    
+    );
+};
+
+export default Error404;
