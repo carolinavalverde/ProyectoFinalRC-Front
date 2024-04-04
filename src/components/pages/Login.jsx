6f0516dd4173c7962394b0f425a34fa23e2ea5d2
Roberto <roberto.v.arebalo@gmail.com>
2024-04-03 21:01:20 -0300

    agrego el componente y queries que corresponden a login

--- src/components/pages/Login.jsx
+++ src/components/pages/Login.jsx
@@ -1,13 +1,90 @@
-// borrar
-import React from 'react';
+import { Form, Button, Container, Card } from "react-bootstrap";
+import Swal from "sweetalert2";
+import { useForm } from "react-hook-form";
+import { login } from "../../helpers/queries";
+import { useNavigate } from "react-router-dom";
 
-const ItemProducto = () => {
+const Login = ({ setUsuarioLogueado }) => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+  const navegacion = useNavigate();
+
+  const onSubmit = (usuario) => {
+    if (login(usuario)) {
+      //soy el admin
+      Swal.fire({
+        title: "Bienvenido",
+        text: "Ingresaste al panel de administración de RollingCoffee",
+        icon: "success",
+      });
+      //guardar el usuario en el state
+      setUsuarioLogueado(usuario.email);
+      //redireccionar al admin
+      navegacion("/administrador");
+    } else {
+      Swal.fire({
+        title: "Ocurrio un error",
+        text: "Email o password incorrecto",
+        icon: "error",
+      });
+    }
+  };
+
   return (
-    <div>
-      <h1>Este es el componente ItemProducto</h1>
-    </div>
+    <Container className="mainSection">
+      <Card className="my-5">
+        <Card.Header as="h5">Login</Card.Header>
+        <Card.Body>
+          <Form onSubmit={handleSubmit(onSubmit)}>
+            <Form.Group className="mb-3" controlId="formBasicEmail">
+              <Form.Label>Email</Form.Label>
+              <Form.Control
+                type="text"
+                placeholder="Ingrese un email"
+                {...register("email", {
+                  required: "El nombre de usuario es obligatorio",
+                  pattern: {
+                    value: /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/,
+                    message:
+                      "Ingrese una dirección de correo electrónico válida",
+                  },
+                })}
+              />
+              <Form.Text className="text-danger">
+                {errors.email?.message}
+              </Form.Text>
+            </Form.Group>
+
+            <Form.Group className="mb-3" controlId="formBasicPassword">
+              <Form.Label>Password</Form.Label>
+              <Form.Control
+                type="password"
+                placeholder="Password"
+                {...register("password", {
+                  required: "El nombre de password es obligatorio",
+                  pattern: {
+                    value: /^(?=.\d)(?=.[a-z])(?=.*[A-Z]).{8,}$/,
+                    message:
+                      "El password debe contener al menos una letra mayúscula, una letra minúscula y un número",
+                  },
+                })}
+              />
+              <Form.Text className="text-danger">
+                {errors.password?.message}
+              </Form.Text>
+            </Form.Group>
+
+            <Button variant="primary" type="submit">
+              Ingresar
+            </Button>
+          </Form>
+        </Card.Body>
+      </Card>
+    </Container>
   );
 };
 
-export default ItemProducto;
-// borrar
+export default Login;
--- src/helpers/queries.js
+++ src/helpers/queries.js
@@ -2,6 +2,7 @@ const APIProductos = import.meta.env.VITE_API_PRODUCTO;
 // const APIProductos = process.env.VITE_API_PRODUCTO;
 console.log(APIProductos);
 
+//GET
 export const leerProductos = async () => {
   try {
     const respuesta = await fetch(APIProductos);
@@ -11,6 +12,7 @@ export const leerProductos = async () => {
   }
 };
 
+//POST
 export const crearProducto = async (productoNuevo) => {
   try {
     const respuesta = await fetch(APIProductos, {
@@ -25,6 +27,78 @@ export const crearProducto = async (productoNuevo) => {
   } catch (error) {
     console.log(error);
   }
+};
 
+//PUT o PATCH
+export const editarProducto = async (productoEditado, id) => {
+  try {
+    const respuesta = await fetch(APIProductos + "/" + id, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(productoEditado),
+    });
+    return respuesta;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+//DELETE
+export const borrarProducto = async (id) => {
+  try {
+    const respuesta = await fetch(APIProductos + "/" + id, {
+      method: "DELETE",
+    });
+    console.log(respuesta);
+    return respuesta;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+//GET de un unico producto
+export const obtenerProducto = async (id) => {
+  try {
+    const respuesta = await fetch(APIProductos + "/"+ id);
+    console.log(respuesta);
+    return respuesta;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+//cuando tengamos el backend con un login enviar solicitud POST
+const userAdmin = {
+  email: "admin@rollingcoffee.com",
+  password: "123Aa$123",
+};
+
+export const login = (usuario) => {
+  if (
+    usuario.email === userAdmin.email &&
+    usuario.password === userAdmin.password
+  ) {
+    sessionStorage.setItem("loginRollingCoffee", JSON.stringify(usuario.email));
+    return true;
+  } else {
+    return false;
+  }
+};
+
+export const registrarUsuario = async (usuario) => {
+  try {
+    const respuesta = await fetch(APIRegistro, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(usuario),
+    });
+    return respuesta;
+  } catch (error) {
+    console.log(error);
+  }
 };
 
