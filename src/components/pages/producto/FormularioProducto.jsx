f25fd3b6f4c396d412a3b05450cb8e7d25a467d0
Caro Valverde <carolina.valverde255@gmail.com>
2024-04-05 14:48:31 -0300

    cargo el form con cada item validado

--- src/components/pages/producto/FormularioProducto.jsx
+++ src/components/pages/producto/FormularioProducto.jsx
@@ -85,13 +85,135 @@ const FormularioProducto = ({ editando, titulo }) => {
   };
 
   return (
-   <section>
-    <form >
-      
-    </form>
-   </section>
+    <section className="container mainSection">
+      <h1 className="display-4 mt-5">{titulo}</h1>
+      <hr />
+      <Form className="my-4" onSubmit={handleSubmit(datosValidados)}>
+        <Form.Group className="mb-3" controlId="formNombreProducto">
+          <Form.Label>Producto*</Form.Label>
+          <Form.Control
+            type="text"
+            placeholder="Ej: Cafe"
+            {...register("nombreProducto", {
+              required: "El nombre del producto es un dato obligatorio",
+              minLength: {
+                value: 4,
+                message:
+                  "El nombre del producto debe tener como mínimo 4 caracteres",
+              },
+              maxLength: {
+                value: 50,
+                message:
+                  "El nombre del producto debe tener como máximo 50 caracteres",
+              },
+            })}
+          />
+          <Form.Text className="text-danger">
+            {errors.nombreProducto?.message}
+          </Form.Text>
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="formPrecio">
+          <Form.Label>Precio*</Form.Label>
+          <Form.Control
+            type="number"
+            placeholder="Ej: 50"
+            {...register("precio", {
+              required: "El precio es un dato obligatorio",
+              min: {
+                value: 100,
+                message: "El precio del producto es como mínimo de $100",
+              },
+              max: {
+                value: 10000,
+                message: "El precio del producto es como máximo de $10000",
+              },
+            })}
+          />
+          <Form.Text className="text-danger">
+            {errors.precio?.message}
+          </Form.Text>
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="formImagen">
+          <Form.Label>Imagen URL*</Form.Label>
+          <Form.Control
+            type="text"
+            placeholder="Ej: https://www.pexels.com/es-es/vans-en-blanco-y-negro-fuera-de-la-decoracion-para-colgar-en-la-pared-1230679/"
+            {...register("imagen", {
+              required: "La url de la imagen es obligatoria",
+              pattern: {
+                value: /^(http(s?):)([/|.|\w|\s|-])*\.(?:jpg|jpeg|gif|png)$/,
+                message:
+                  "Debe ingresar una URL válida, con una imagen en formato (jpg | jpeg | gif | png)",
+              },
+            })}
+          />
+          <Form.Text className="text-danger">
+            {errors.imagen?.message}
+          </Form.Text>
+
+          <Form.Label>Categoría*</Form.Label>
+          <Form.Select
+            {...register("categoria", {
+              required: "Debe seleccionar una categoría",
+            })}
+          >
+            <option value="">Seleccione una opcion</option>
+            <option value="Infusiones">Infusiones</option>
+            <option value="Batidos">Batidos</option>
+            <option value="Dulce">Dulce</option>
+            <option value="Salado">Salado</option>
+          </Form.Select>
+          <Form.Text className="text-danger">
+            {errors.categoria?.message}
+          </Form.Text>
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="formDescripcionBreve">
+          <Form.Label>Descripción breve*</Form.Label>
+          <Form.Control
+            type="text"
+            placeholder="Ej: Una taza de café suave y aromático."
+            as="textarea"
+            {...register("descripcion_breve", {
+              required: "La descripcion breve es obligatoria",
+              minLength: {
+                value: 10,
+                message: "Debe ingresar como minimo 10 caracteres",
+              },
+              maxLength: {
+                value: 50,
+                message: "Debe ingresar como maximo 50 caracteres",
+              },
+            })}
+          />
+          <Form.Text className="text-danger">
+            {errors.descripcion_breve?.message}
+          </Form.Text>
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="formDescripcionAmplia">
+          <Form.Label>Descripción Amplia*</Form.Label>
+          <Form.Control
+            type="text"
+            placeholder="Ej: El café americano es una bebida caliente que consiste en un espresso diluido con agua caliente, lo que resulta en una taza de café suave y aromático. Es una opción popular para aquellos que prefieren un café menos intenso que el espresso tradicional. Perfecto para disfrutar en cualquier momento del día."
+            as="textarea"
+            {...register("descripcion_amplia", {
+              required: "La descripcion amplia es obligatoria",
+              minLength: {
+                value: 30,
+                message: "Debe ingresar como minimo 30 caracteres",
+              },
+              maxLength: {
+                value: 300,
+                message: "Debe ingresar como maximo 300 caracteres",
+              },
+            })}
+          />
+          <Form.Text className="text-danger">
+            {errors.descripcion_amplia?.message}
+          </Form.Text>
+        </Form.Group>
+      </Form>
+    </section>
   );
 };
 
 export default FormularioProducto;
-
