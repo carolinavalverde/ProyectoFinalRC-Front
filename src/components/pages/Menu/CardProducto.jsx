069e52ffb8f90ccf1c0f284bc359e164015892be
santiago <santtyzelada@gmail.com>
2024-04-09 19:45:09 +0200

    Agregamos la ID a cada item del compon. CardProducto.jsx

--- src/components/pages/Menu/CardProducto.jsx
+++ src/components/pages/Menu/CardProducto.jsx
@@ -1,21 +1,34 @@
 import React from "react";
-import "../../../styles/Card.css"
-import img1 from "../../img/bebidas1.avif"
+import "../../../styles/Card.css";
+import { Link } from "react-router-dom";
 
-const CardProducto = (props) => {
-
-    const {title} = props
-
+const CardProducto = ({ producto }) => {
     return (
-        <div className="cardContainer">
-            <h1>{title}</h1>
-            <div className="card">
-                <div className="image"><img src={img1} alt="Bebidas" style={{ width: '100%', height: '100%' }} /></div>
-                <span className="title">Dish name</span>
-                <span className="price">$100</span>
-            </div>
-        </div>
-    )
-}
+        <section className="cardContainer">
+            <h1>{producto.nombreProducto}</h1>
+            <section className="card">
+                <div className="image">
+                    <img
+                        src={producto.imagen}
+                        alt={producto.nombreProducto}
+                        style={{ width: "100%", height: "100%" }}
+                    />
+                </div>
+                <div>
+                    <h2 className="title">{producto.nombreProducto}</h2>
+                    <span className="price">{producto.precio}</span>
+                </div>
+                <div className="d-flex justify-content-center">
+                    <Link
+                        className="btn btn-success me-2"
+                        to={`/detalleproducto/${producto.id}`}
+                    >
+                        Ver más
+                    </Link>
+                </div>
+            </section>
+        </section>
+    );
+};
 
 export default CardProducto;
