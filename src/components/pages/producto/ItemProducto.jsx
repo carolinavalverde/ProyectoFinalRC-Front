c2d00f56b67059ed340e7632ae2d325a74b09bcb
Figueroa Fernando <feroa88@gmail.com>
2024-04-10 15:53:29 -0300

    agrego boton y link de navegacion

--- src/components/pages/producto/ItemProducto.jsx
+++ src/components/pages/producto/ItemProducto.jsx
@@ -3,7 +3,10 @@ import Swal from "sweetalert2";
 import { borrarProducto, leerProductos } from "../../../helpers/queries";
 import { Link } from "react-router-dom";
 
-const ItemProducto = ({ producto, setProductos }) => {
+const ItemProducto = ({producto, setProductos}) => {
+
+  
+
   return (
     <tr>
       <td className="text-center">{producto._id}</td>
@@ -17,8 +20,16 @@ const ItemProducto = ({ producto, setProductos }) => {
         ></img>
       </td>
       <td>{producto.categoria}</td>
+      <td className="text-center">
+        <Link className="me-lg-2 btn btn-warning" to={'/administrador/editar/'+ producto._id}>
+          <i className="bi bi-pencil-square"></i>
+        </Link>
+        <Button variant="danger" onClick={eliminarProducto}>
+          <i className="bi bi-trash"></i>
+        </Button>
+      </td>
     </tr>
   );
 };
 
-export default ItemProducto;
+export default ItemProducto;
