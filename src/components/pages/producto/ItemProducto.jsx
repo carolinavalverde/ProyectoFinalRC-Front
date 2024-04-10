81728d5ceb02468ddaebb8911133b95170882f30
Figueroa Fernando <feroa88@gmail.com>
2024-04-10 15:52:15 -0300

    agrego maquetado de el componente

--- src/components/pages/producto/ItemProducto.jsx
+++ src/components/pages/producto/ItemProducto.jsx
@@ -1,9 +1,24 @@
-import React from "react";
+import { Button } from "react-bootstrap";
+import Swal from "sweetalert2";
+import { borrarProducto, leerProductos } from "../../../helpers/queries";
+import { Link } from "react-router-dom";
 
-const ItemProducto = () => {
-    return (
-        <div> <h1>Hello World!</h1>  </div>
-    )
-}
+const ItemProducto = ({ producto, setProductos }) => {
+  return (
+    <tr>
+      <td className="text-center">{producto._id}</td>
+      <td>{producto.nombreProducto}</td>
+      <td className="text-end">${producto.precio}</td>
+      <td className="text-center">
+        <img
+          src={producto.imagen}
+          className="img-thumbnail"
+          alt={producto.nombreProducto}
+        ></img>
+      </td>
+      <td>{producto.categoria}</td>
+    </tr>
+  );
+};
 
 export default ItemProducto;
