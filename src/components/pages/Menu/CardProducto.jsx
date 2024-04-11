89b184b72024ec2080b53edf6b38fe7beb37383c
santiago <santtyzelada@gmail.com>
2024-04-11 14:22:14 +0200

    Corregí app.jsx, la card, el menucatgoria y el dbjson

--- src/components/pages/Menu/CardProducto.jsx
+++ src/components/pages/Menu/CardProducto.jsx
@@ -1,32 +1,36 @@
 import React from "react";
+import { Card, Col, Row } from "react-bootstrap";
 import "../../../styles/Card.css";
 import { Link } from "react-router-dom";
 
 const CardProducto = ({ producto }) => {
     return (
-        <section className="cardContainer">
-            <h1>{producto.nombreProducto}</h1>
-            <section className="card">
-                <div className="image">
-                    <img
-                        src={producto.imagen}
-                        alt={producto.nombreProducto}
-                        style={{ width: "100%", height: "100%" }}
-                    />
-                </div>
-                <div>
-                    <h2 className="title">{producto.nombreProducto}</h2>
-                    <span className="price">{producto.precio}</span>
-                </div>
-                <div className="d-flex justify-content-center">
-                    <Link
-                        className="btn btn-success me-2"
-                        to={`/detalleproducto/${producto.id}`}
-                    >
-                        Ver más
-                    </Link>
-                </div>
-            </section>
+        <section className="container my-3">
+            <Card className="bg-opacity-25 bg-black border-0">
+                <Row className="gx-0">
+                    <Col md={4}>
+                        <div className="bg-dark bg-opacity-25 d-flex justify-content-center">
+                            <img
+                                src={producto.imagen}
+                                alt={producto.nombreProducto}
+                                className="card-img img-fluid imagenCardProducto"
+                            />
+                        </div>
+                    </Col>
+                    <Col md={8}>
+                        <Card.Body>
+                            <Card.Title className="TituloCardProducto fs-2">{producto.nombreProducto}</Card.Title>
+                            <Card.Text className="TextoCardProducto fs-5">Precio: {producto.precio}</Card.Text>
+                            <Link
+                                className="btn btn-light me-2"
+                                to={`/detalleproducto/${producto.id}`}
+                            >
+                                Ver más
+                            </Link>
+                        </Card.Body>
+                    </Col>
+                </Row>
+            </Card>
         </section>
     );
 };
--- src/components/pages/Menu/NuestrosProductos.jsx
+++ src/components/pages/Menu/NuestrosProductos.jsx
@@ -1,68 +1,55 @@
 import "../../../App.css";
-import MenuCategorias from "./MenuCategorias"
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "bootstrap/dist/css/bootstrap.min.css";
 import CarouselPage from "./CarouselPage";
 import CardProducto from "./CardProducto";
 import { Container, Row } from "react-bootstrap";
 import { leerProductos } from "../../../helpers/queries";
-import { useEffect, useState, React } from "react";
+import { useEffect, useState } from "react";
+import Swal from "sweetalert2";
 
 const NuestrosProductos = () => {
     const [productos, setProductos] = useState([]);
 
     useEffect(() => {
-        //solicitar a la api traer los productos
         obtenerProductos();
     }, []);
 
     const obtenerProductos = async () => {
-        const respuesta = await leerProductos();
-        if (respuesta.status === 200) {
-            const datos = await respuesta.json();
-            setProductos(datos);
-        } else {
-            //mostrar un mjs elegante de error al usuario de q en este momento no puede hacer esta transaccion
+        try {
+            const respuesta = await leerProductos();
+            if (respuesta.status === 200) {
+                const datos = await respuesta.json();
+                setProductos(datos);
+            } else {
+                throw new Error("Error en la solicitud");
+            }
+        } catch (error) {
+            Swal.fire({
+                title: "Ocurrió un error",
+                text: `No se pueden obtener los productos. Intente esta operación en unos minutos.`,
+                icon: "error",
+            });
         }
     };
 
-    console.log(productos);
-
-
     return (
-
         <div>
             <div className="itemsContainer">
                 <hr />
-
-                <CarouselPage></CarouselPage>
+                <CarouselPage />
                 <hr />
-
-                {/* <h2 style={{ fontWeight: '100', fontSize: '3rem', textTransform: 'uppercase' }}>Entradas</h2>
-                <MenuCategorias />
-                <hr />
-                <h2 style={{ fontWeight: '100', fontSize: '3rem', textTransform: 'uppercase' }}>Platillos</h2>
-                <MenuCategorias />
-                <hr />
-
-                <h2 style={{ fontWeight: '100', fontSize: '3rem', textTransform: 'uppercase' }}>Bebidas</h2>
-                <MenuCategorias />
-                <hr />
-
-                <h2 style={{ fontWeight: '100', fontSize: '3rem', textTransform: 'uppercase' }}>Postres</h2>
-                <MenuCategorias /> */}
                 <Container className="mt-5">
-                    <h1 className="display-4">Nuestros Productos</h1>
+                    <h1 className="display-4 text-center">Nuestros Productos</h1>
                     <hr />
-
                     <Row>
-                        {productos.slice(0, 3).map((producto) => (
-                            <CardProducto key={producto._id} producto={producto}></CardProducto>
+                        {productos.map((producto) => (
+                            <CardProducto key={producto.id} producto={producto} />
                         ))}
                     </Row>
                 </Container>
             </div>
         </div>
-    )
-}
+    );
+};
 
 export default NuestrosProductos;
