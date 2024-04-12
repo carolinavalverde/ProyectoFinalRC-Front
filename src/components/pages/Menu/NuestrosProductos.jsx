1bc87dabc45d029027ec462990ed4d6004988836
Roberto <roberto.v.arebalo@gmail.com>
2024-04-12 19:07:56 -0300

    modifico llave del producto

--- src/components/pages/Menu/NuestrosProductos.jsx
+++ src/components/pages/Menu/NuestrosProductos.jsx
@@ -43,7 +43,7 @@ const NuestrosProductos = () => {
                     <hr />
                     <Row>
                         {productos.map((producto) => (
-                            <CardProducto key={producto.id} producto={producto} />
+                            <CardProducto key={producto._id} producto={producto} />
                         ))}
                     </Row>
                 </Container>
