04184af462b7d047bd2505ab80a0af11edaa47b6
Caro Valverde <carolina.valverde255@gmail.com>
2024-04-13 12:27:51 -0300

    agrego categorias e imagen en css del  inicio

--- src/components/pages/Inicio.jsx
+++ src/components/pages/Inicio.jsx
@@ -3,27 +3,32 @@ import { Link } from "react-router-dom";
 import "../../styles/Inicio.css";
 import CategoriaInicio from "./CategoriaInicio";
 
-import platillosImage from "../assets/img/platillos1.avif";
-import bebidasImage from "../assets/img/bebidas1.jpg";
-import postresImage from "../assets/img/postres1.avif";
-
 const Inicio = () => {
   return (
     <section>
       <div className="contenedorRestaurantVermont">
-        <div className="display-3">Restaurant Vermont</div>
+        <div className="display-2 text-center mb-2">Restaurant Vermont</div>
         <Link className="btnNuestrosProductos" to="/nuestrosproductos">
           Nuestros Productos
         </Link>
       </div>
       <div>
         <section className="imgCategoriaEntradas">
-          <div  />
-          <div className="textoImgCategoria text-center mb-2">Entradas</div>
+          <div className="textoImgCategoria text-center display-2">
+            Entradas
+          </div>
         </section>
-        <div title="Platillos" backgroundImage={platillosImage} />
-        <CategoriaInicio title="Bebidas" backgroundImage={bebidasImage} />
-        <CategoriaInicio title="Postres" backgroundImage={postresImage} />
+        <section className="imgCategoriaPlatillos">
+          <div className="textoImgCategoria text-center display-2">
+            Platillos
+          </div>
+        </section>
+        <section className="imgCategoriaBebidas">
+          <div className="textoImgCategoria text-center display-2">Bebidas</div>
+        </section>
+        <section className="imgCategoriaPostres">
+          <div className="textoImgCategoria text-center display-2">Postres</div>
+        </section>
       </div>
     </section>
   );
