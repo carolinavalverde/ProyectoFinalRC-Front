2c069b360eb818f5078ed1ea3f9cc2ff5cca16b3
Caro Valverde <carolina.valverde255@gmail.com>
2024-04-13 12:14:50 -0300

    agrego css categorias

--- src/components/pages/Inicio.jsx
+++ src/components/pages/Inicio.jsx
@@ -2,43 +2,31 @@ import React from "react";
 import { Link } from "react-router-dom";
 import "../../styles/Inicio.css";
 import CategoriaInicio from "./CategoriaInicio";
-import entradasImage from "../assets/img/entradas1.avif";
+
 import platillosImage from "../assets/img/platillos1.avif";
 import bebidasImage from "../assets/img/bebidas1.jpg";
 import postresImage from "../assets/img/postres1.avif";
 
 const Inicio = () => {
-    return (
-        <section>
-            <div className="contenedorRestaurantVermont">
-                <div>Restaurant Vermont</div>
-                <Link
-                    className="btnNuestrosProductos"
-                    to="/nuestrosproductos"
-                >
-                    Nuestros Productos
-                </Link>
-            </div>
-            <div>
-                <CategoriaInicio
-                    title="Entradas"
-                    backgroundImage={entradasImage}
-                />
-                <CategoriaInicio
-                    title="Platillos"
-                    backgroundImage={platillosImage}
-                />
-                <CategoriaInicio
-                    title="Bebidas"
-                    backgroundImage={bebidasImage}
-                />
-                <CategoriaInicio
-                    title="Postres"
-                    backgroundImage={postresImage}
-                />
-            </div>
+  return (
+    <section>
+      <div className="contenedorRestaurantVermont">
+        <div className="display-3">Restaurant Vermont</div>
+        <Link className="btnNuestrosProductos" to="/nuestrosproductos">
+          Nuestros Productos
+        </Link>
+      </div>
+      <div>
+        <section className="imgCategoriaEntradas">
+          <div  />
+          <div className="textoImgCategoria text-center mb-2">Entradas</div>
         </section>
-    );
+        <div title="Platillos" backgroundImage={platillosImage} />
+        <CategoriaInicio title="Bebidas" backgroundImage={bebidasImage} />
+        <CategoriaInicio title="Postres" backgroundImage={postresImage} />
+      </div>
+    </section>
+  );
 };
 
 export default Inicio;
